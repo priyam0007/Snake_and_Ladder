edd178cd3038941c1444dddcca1f6bdacfa705ff
Priya <Priyamadiwalar0007@gmail.com>
2022-02-07 04:03:24 +0530

    added: SnakeAndLadder

--- src/com/sankeandladder/SnakeAndLadder.java
+++ src/com/sankeandladder/SnakeAndLadder.java
@@ -6,11 +6,14 @@
     			  
     			
     			       int Position=0;
-   
+    			      
+    			       int roll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1 );
     			
-                  System.out.println("Welcome to Snake and Ladder game");
+                    System.out.println("Welcome to Snake and Ladder game");
     			
     			       System.out.println("You are at Starting Position::"+Position);
+    			       
+    			          System.out.println(roll);
     			
     			}
 
