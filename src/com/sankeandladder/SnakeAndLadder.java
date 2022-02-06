61f9ed1030665b11b4e4930052116516966bba3d
Priya <Priyamadiwalar0007@gmail.com>
2022-02-07 03:35:06 +0530

    added: SnakeAndLadder

---
+++ src/com/sankeandladder/SnakeAndLadder.java
@@ -0,0 +1,19 @@
+  package com.sankeandladder;
+
+      public class SnakeAndLadder {
+    	  
+    		  public static void main(String[] args) {
+    			  
+    				int Position = 0;
+    				
+
+                  System.out.println("Welcome to Snake and Ladder game");
+                  
+                       System.out.println("You are at Starting Position::"+Position);
+    		}
+    		
+
+		}
+	
+
+
